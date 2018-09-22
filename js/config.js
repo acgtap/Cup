fcdd89fd29d88d09bc2f24ffe7d87846fa0c98df
林檎 <40010177+acgtap@users.noreply.github.com>
2018-09-22 12:52:01 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,17 +1,9 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	'u633808-954ece59e6a7bb6b03f4a606', //Community
-	'm776858325-1ddf7c7e98ce56609325db3b', //Document
-	'm776858327-19b31e671f6b217b8ded96c6', //Me
-	'm776858407-a704cd548fb5c15db5b625f9', //Update A
-	'm778126978-24e6f1ce275c2518bad1e925', //Update B
-	'm777157148-a67f71b8a807c54b8ce92c9d', //Update deliver A
-	'm777157137-ce781489d1af15253ca3f0c1', //Update deliver B
-	'm776858320-7b44f9ac136f69ac3e356e8e', //Auth A
-	'm776858321-486827042ed7a7f7b8952a05', //Auth B
-	'm778734196-a51947f061c5db10cee048f7', //Auth C
-	'm776858323-0bdafb9e9a71e3305c27c1af', //Auth D
-	'm777190463-ac0ab733e1b29c92b7de0354', //Auth E
+	'm781008986-612b9c3d73d5ae98af8daa28', //首页
+	'm781008989-d733d5cad6067bd47e953910', //接口
+	'm781008987-a77ea08c1726017c2f091e9c', //菜单
+	'm781008994-10980b10ef52cb2ddf0c895d',
 ];
 
 // refresh interval (in seconds)
