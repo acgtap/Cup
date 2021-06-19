c0225d736d3b90b2823d8573ad4d11beabf1641c
林檎 <40010177+acgtap@users.noreply.github.com>
2021-06-19 14:50:01 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	'm781008986-612b9c3d73d5ae98af8daa28', //首页
+	'954ece59e6a7bb6b03f4a606', //首页
 	'm781008989-d733d5cad6067bd47e953910', //接口
 	'm781008987-a77ea08c1726017c2f091e9c', //菜单
 	'm781008994-10980b10ef52cb2ddf0c895d',
